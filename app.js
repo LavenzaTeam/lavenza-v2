b34733ad667949d9e753d278c7ad76c9d7df41cc
ArkaneOnline <niflheim.exease@gmail.com>
2019-12-01 22:07:05 -0600

    fix error

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const client = new Client();
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
 
-client.login("NjM5OTg4MTk0MjI5ODc4ODEw.XdtqWA.Wwhl3iGEpxsL_QGfUZi3SebdO_c");
+client.login();
