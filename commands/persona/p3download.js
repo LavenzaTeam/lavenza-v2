9a06ddc4d888c73cbc8d2fc73aafb061316022e9
ArkaneOnline <niflheim.exease@gmail.com>
2019-12-02 15:09:48 -0600

    add p3 download

---
+++ commands/persona/p3download.js
@@ -0,0 +1,39 @@
+const { RichEmbed } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    config: {
+        name: "p3download",
+        aliases: ["p3dl", "p3emulation", "p3emulate"],
+        description: "Sends a message with links for Emulators and ROMs for all the Persona 3 games",
+        usage: "",
+        category: "persona"
+    },
+    run: async (client, message, args) => {
+        let ps2emu = "https://pcsx2.net/";
+        let pspemu = "https://ppsspp.org/";
+        let ps3emu = "https://rpcs3.net";
+        let p3fes = "https://coolrom.com/roms/ps2/41813/Shin_Megami_Tensei_-_Persona_3_FES.php";
+        let p3p = "https://www.freeroms.com/roms/psp/shin_megami_tensei_-_persona_3_portable.htm";
+        let p3ps3 = "Nothing here at the moment. ¯\\_(ツ)_/¯";
+
+        let p3embed = new RichEmbed()
+        .setTitle("Persona 3")
+        .setDescription("Below is a list of all the emulators and ROMs to play the Persona 3 games!")
+        .setColor(config.colors.blue)
+        .addField("PlayStation 2 Emulator", ps2emu)
+        .addField("PlayStation Portable Emulator", pspemu)
+        .addField("PlayStation 3 Emulator", ps3emu)
+        .addBlankField(true)
+        .addField("Persona 3 FES (PS2)", p3fes)
+        .addField("Persona 3 Portable (PSP)", p3p)
+        .addField("Persona 3 FES (PS2 Classic on PS3)", p3ps3)
+        .addBlankField(true)
+        .addField("Note", "If one of the links decide to not work, Please join the support server and tell me in there! `;support`")
+        .setTimestamp()
+        .setFooter("Created by Arkane", client.users.get(config.ids.arkane).displayAvatarURL)
+
+        
+        message.channel.send(p3embed);
+    }
+}
