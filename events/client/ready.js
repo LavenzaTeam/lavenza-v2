86baf46ddda7602bc56c68b6ca16406030fce07f
ArkaneOnline <niflheim.exease@gmail.com>
2019-12-24 00:16:43 -0600

    add more presences

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,6 +2,13 @@ const { prefix } = require("../../config.json");
 
 module.exports = async (client) => {
     let presences = [
+        "...You are held captive. A prisoner of fate to a future that has been sealed in advance.",
+        "This is truly an unjust game... Your chances of winning are almost none.",
+        "But if my voice is reaching you, there may yet be a possibility open to you...",
+        "...I beg you. Please overcome this game... and save the world...",
+        "The key to victory lies within the memories of your bonds—the truth that you and your friends grasped.",
+        "It all began that day... when the game was started half a year ago...",
+        "For the sake of your world's future... as well as your own... you must remember...",
         "Fusion Calculator Soon!",
         `${prefix}ping`,
         `${prefix}invite`,
