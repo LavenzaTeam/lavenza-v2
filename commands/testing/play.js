072d9cd7351221417baa03b7fd7a6a11489a33c5
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-19 19:42:09 -0600

    test out some music

---
+++ commands/testing/play.js
@@ -0,0 +1,23 @@
+const ytdl = require("ytdl-core");
+
+module.exports = {
+    config: {
+        name: "play",
+        aliases: ["p"],
+        description: "A test command for the music functionality coming soon to Lavenza!",
+        usage: "",
+        category: "testing"
+    },
+    run: async (client, message, args) => {
+        if (message.channel.type !== "text") return message.reply("Error, non valid text channel!");
+        const { voiceChannel } = message.member;
+
+        if(!voiceChannel) return message.reply("You must be in a Voice Channel to use this command!");
+        message.reply("Joining your Voice Channel! Playing Keep your Faith!");
+        voiceChannel.join().then(connection => {
+            const stream = ytdl("https://www.youtube.com/watch?v=xTH2g1JQxXc&list=PL1Ujbrns4Hh55A7xfDrrk_AXC3I_Bbkyx&index=24", { filter: "audioonly" });
+            const dispatcher = connection.playStream(stream);
+            dispatcher.on("end", () => voiceChannel.leave());
+        })
+    }
+}
