3a9d203805d527c0d8c1fa035c97a8ca380fa2c5
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-19 20:44:41 -0600

    update play command to be better

--- commands/testing/play.js
+++ commands/testing/play.js
@@ -9,15 +9,28 @@ module.exports = {
         category: "testing"
     },
     run: async (client, message, args) => {
+        //generic checks and definitions
         if (message.channel.type !== "text") return message.reply("Error, non valid text channel!");
         const { voiceChannel } = message.member;
 
+        //argument checking
         if(!voiceChannel) return message.reply("You must be in a Voice Channel to use this command!");
-        message.reply("Joining your Voice Channel!");
-        voiceChannel.join().then(connection => {
-            const stream = ytdl('https://www.youtube.com/watch?v=Kbw6hF7tHns', { filter: 'audioonly' });
-			const dispatcher = connection.playStream(stream);
+        if(message.guild.me.voiceChannel) return message.reply("Sorry, I am already in another Voice Channel in this server!");
+        let url = "https://www.youtube.com/watch?v=Kbw6hF7tHns";
+        if(!url) return message.reply("No URL provided!");
 
+        //validation checking
+        let validate = await ytdl.validateURL(url);
+        if(!validate) return message.reply("Please input a valid YouTube URL!");
+
+        //grab video info
+        let info = await ytdl.getInfo(url);
+
+        //establish the connection to the voice channel
+        voiceChannel.join().then(connection => {
+            const stream = ytdl(url, { filter: 'audioonly' });
+			const dispatcher = connection.playStream(stream);
+            message.channel.send(`Now playing: **${info.title}**`);
 			dispatcher.on('end', () => voiceChannel.leave());
         })
     }
