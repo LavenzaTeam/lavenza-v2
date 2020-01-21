34d4f0964f4a867419e580fb84ee033cb94f674e
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-21 00:49:04 -0600

    fix now playing

--- commands/testing/music.js
+++ commands/testing/music.js
@@ -68,7 +68,7 @@ module.exports = {
         async function playSong(messageChannel, voiceConnection, voiceChannel) {
             const stream = ytdl(musicURLs[0], { filter: "audioonly" });
             const dispatcher = voiceConnection.playStream(stream);
-            message.channel.send(`Now playing: **${info.title}**`);
+            messageChannel.send(`Now playing: **${info.title}**`);
             if(!voiceConnection) dispatcher.end(); 
 
             dispatcher.on("end", () => {
