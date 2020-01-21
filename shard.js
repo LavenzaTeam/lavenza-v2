66235c9c04a69ff3f6d2df09d27d4d4a3501cf89
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-21 15:58:39 -0600

    new shard count test

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./app.js", { token: process.env.token });
 
-manager.spawn(2);
+manager.spawn(5, 5000);
 manager.on("launch", shard => console.log(`Shard #${shard.id} launched successfully!`));
