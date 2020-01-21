26f030d22d8daa69d0f672fcf44893b85153b211
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-21 15:47:19 -0600

    better logging

--- events/guild/message.js
+++ events/guild/message.js
@@ -6,6 +6,7 @@ module.exports = async (client, message) => {
 
     let args = message.content.slice(prefix.length).trim().split(/ +/g)
     let cmd = args.shift().toLowerCase();
+    if(!args) args = null;
 
     if(!message.content.startsWith(prefix)) return;
     let commandfile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd))
