ffd31e854a40a1220f48c296b29e1cbfb5d67995
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-23 14:48:25 -0600

    add avatar command

---
+++ commands/general/avatar.js
@@ -0,0 +1,34 @@
+const { RichEmbed, Attachment } = require("discord.js");
+const config = require("discord.js");
+
+module.exports = {
+    config: {
+        name: "avatar",
+        aliases: ["pfp", "profilepic"],
+        description: "Sends an image of your profile picture!",
+        usage: "(@user)",
+        category: "general"
+    },
+    run: async (client, message, args) => {
+        //stores the user that you pinged in a variable
+        let aUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+        //checks if you pinged somebody, if you did, it displays their avatar, otherwise, it displays you avatar
+        if (aUser) {
+            var userURL = aUser.user.avatarURL;
+            var image = new Attachment(userURL);
+        } else {
+            var yourURL = message.author.avatarURL;
+            var image = new Attachment(yourURL);
+        }
+
+        let embed = new RichEmbed()
+            .setTitle(aUser)
+            .setDescription(`${aUser}'s Discord Avatar`)
+            .setImage(image)
+            .setFooter(`Data requested by ${message.author.name}`, message.author.displayAvatarURL);
+
+        //stops the code in its tracks
+        return message.channel.send(embed);
+    }
+}
