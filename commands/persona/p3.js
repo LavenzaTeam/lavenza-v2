5a444b10a119bd800c6fdd7d78e4017c41dfa7d4
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-24 15:47:21 -0600

    maybe?

--- commands/persona/p3.js
+++ commands/persona/p3.js
@@ -1,8 +1,3 @@
-let p3pcommand = client.commands.get("p3personas");
-let p3scommand = client.commands.get("p3shadows");
-let p3ffcommand = client.commands.get("p3fesfusion");
-let p3pfcommand = client.commands.get("p3pfusion");
-
 module.exports = {
     config: {
         name: "p3",
@@ -12,6 +7,10 @@ module.exports = {
         category: "persona"
     },
     run: async (client, message, args) => {
+        let p3pcommand = client.commands.get("p3personas");
+        let p3scommand = client.commands.get("p3shadows");
+        let p3ffcommand = client.commands.get("p3fesfusion");
+        let p3pfcommand = client.commands.get("p3pfusion");
         let variable = args.join("_").toLowerCase();
 
         if (variable === "orpheus") {
