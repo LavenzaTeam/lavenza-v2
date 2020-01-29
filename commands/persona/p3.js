5fba53b30bd3e716faa7c2007fc618df1160a8e2
ArkaneOnline <niflheim.exease@gmail.com>
2020-01-29 00:25:06 -0600

    update p3 command for switch cases and list command

--- commands/persona/p3.js
+++ commands/persona/p3.js
@@ -13,16 +13,41 @@ module.exports = {
         let p3pfcommand = client.commands.get("p3pfusion");
         let query = args.join("_").toLowerCase();
 
+        //checks for the "list" argument
+        if (query === "list") {
+            return message.reply("The list for the `P3` command is currently a Work in Progress! Check back soon!");
+        }
+
         //p3personas command file
-        if(query === "orpheus") p3pcommand.run(client, message, args, query);
+        switch (query) {
+            //fool arcana
+            case "orpheus":
+                p3pcommand.run(client, message, args, query);
+                break;
+        }
 
         //p3shadows command file
-        if(query === "maya") p3scommand.run(client, message, args, query);
+        switch (query) {
+            //maya shadows
+            case "maya":
+                p3scommand.run(client, message, args, query);
+                break;
+        }
 
         //p3fesfusion command file
-        if(query === "orpheus_/f") p3ffcommand.run(client, message, args, query);
+        switch (query) {
+            //fool arcana
+            case "orpheus_/f":
+                p3ffcommand.run(client, message, args, query);
+                break;
+        }
 
         //p3pfusion command file
-        if(query === "orpheus_/p") p3pfcommand.run(client, message, args, query);
+        switch (query) {
+            //fool arcana
+            case "orpheus_/p":
+                p3pfcommand.run(client, message, args, query);
+                break;
+        }
     }
 }
