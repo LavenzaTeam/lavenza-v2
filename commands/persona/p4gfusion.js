a3c0eee2dae8c0a85c3dddc4bfcd67c872258ba6
ArkaneOnline <niflheim.exease@gmail.com>
2020-02-10 02:05:11 -0600

    add p4g fusion command

---
+++ commands/persona/p4gfusion.js
@@ -0,0 +1,23 @@
+const { RichEmbed } = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+    config: {
+        name: "p4fusion",
+        aliases: ["p4f"],
+        description: "A command that sends data for every fusion in Persona 4 The Golden",
+        usage: "<persona>",
+        category: "persona"
+    },
+    run: async (client, message, args, query) => {
+        //general data parsing
+        let personaName = args.join("_").toLowerCase();
+        if (query) personaName = query;
+        if (personaName.endsWith("/f")) personaName = personaName.slice(0, -3);
+
+        //checks if a persona was specified by the user
+        if (!personaName) return message.reply(`You have not specified a Persona! Check \`${config.prefix}p4gfusion list\` for a list of valid Persona's!`);
+
+        return message.reply("Command currently under construction!");
+    }
+}
