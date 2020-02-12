edbdfea79984e90038c500482e74296cf930b582
ArkaneOnline <niflheim.exease@gmail.com>
2020-02-12 09:08:45 -0600

    test new embed

--- commands/general/helpv2.js
+++ commands/general/helpv2.js
@@ -17,23 +17,35 @@ module.exports = {
         if (!command) {
             let helpEmbed = new RichEmbed()
                 .setTitle("List of Commands")
+                .setThumbnail(client.user.displayAvatarURL)
                 .setDescription(`Below is a list of commands for ${client.user.username}! To view more info about a specific command, type \`${config.prefix}help <command>\``)
                 .setColor(config.colors.darkblue)
                 .addField("> General Commands [4]", "`help`, `helpv2`, `invite`, `support`")
                 .addField("> Info Commands [2]", "`ping`, `version`")
                 .addField("> Fun Commands [1]", "`avatar`")
                 .addField("> Moderation Commands [3]", "`kick`, `ban`, `clear`")
-                .addField("> Persona Commands [3]", "`p3`, `p3fesfusion`, `p3pfusion`")
+                .addField("> Persona Commands [1]", "`p3`")
                 .setFooter("Bot created by: Arkane, QueenofPiking, and CoolStalker", client.user.displayAvatarURL)
                 .setTimestamp();
 
             return message.channel.send(helpEmbed);
         } else {
             switch (command) {
-                case "":
-
+                case "help":
+                    var embed = new RichEmbed()
+                        .setTitle("Help Command")
+                        .setColor(config.colors.purple)
+                        .setThumbnail(client.user.displayAvatarURL)
+                        .setDescription("A command that tells you about other commands!")
+                        .addField("Usage", `${config.prefix}help (command)`)
+                        .addField("Category", "General")
+                        .addField("Aliases", `${config.prefix}hv2`)
+                        .setFooter("<> = required | () = optional", client.user.displayAvatarURL)
+                        .setTimestamp();
                     break;
             }
+
+            return message.channel.send(embed);
         }
     }
 }
