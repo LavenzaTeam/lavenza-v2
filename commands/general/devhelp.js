7572e4c400330b3cbff62601bbd18d4312cb62ab
ArkaneOnline <niflheim.exease@gmail.com>
2020-02-13 00:40:47 -0600

    mess with dev help command

--- commands/general/devhelp.js
+++ commands/general/devhelp.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     run: async (client, message, args) => {
         let dev = config.ids;
-        if(message.author.id !== dev.arkane || dev.pike || dev.coolstalker) return message.reply("Test works chief.");
+        if(message.author.id === dev.arkane || dev.pike || dev.coolstalker) return message.reply("Test works chief.");
 
         //im not gonna explain this, you don't need to fuck with this anyways
         const embed = new RichEmbed()
