1ab631fd53d5abbd0635947108034893c8af99e7
ArkaneOnline <niflheim.exease@gmail.com>
2020-02-13 00:28:08 -0600

    change some configurations around

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,22 +3,12 @@ const package = require("../../package.json");
 
 module.exports = async (client) => {
     let presences = [
-        "...You are held captive. A prisoner of fate to a future that has been sealed in advance.",
-        "This is truly an unjust game... Your chances of winning are almost none.",
-        "But if my voice is reaching you, there may yet be a possibility open to you...",
-        "...I beg you. Please overcome this game... and save the world...",
-        "The key to victory lies within the memories of your bonds—the truth that you and your friends grasped.",
-        "It all began that day... when the game was started half a year ago...",
-        "For the sake of your world's future... as well as your own... you must remember...",
-        "Fusion Calculator Soon!",
-        `${prefix}ping`,
+        `${prefix}p3`,
+        `Version ${package.version}`,
+        `Currently serving ${client.guilds.size} servers!`,
+        `${client.users.size} people reading the compendium!`,
         `${prefix}invite`,
-        `${prefix}support`,
-        `${prefix}p3personas`,
-        `${prefix}p3shadows`,
-        `${prefix}p3summary`,
-        `${prefix}p3fesfusion`,
-        `${prefix}p3pfusion`
+        `${prefix}support`
     ]
 
     setInterval(() => {
