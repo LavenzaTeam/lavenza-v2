c2e139ec1a10c10316635d2cc4aaae53a10c41af
ArkaneOnline <niflheim.exease@gmail.com>
2020-04-23 18:56:42 -0500

    test

--- commands/general/help.js
+++ commands/general/help.js
@@ -23,7 +23,6 @@ module.exports = {
                 .addField("> General Commands [4]", "`help`, `invite`, `support`, `patreon`, `avatar`")
                 .addField("> Persona Commands [1]", "`p3`")
                 .setFooter("Bot created by: Arkane, QueenofPiking, and CoolStalker", client.user.displayAvatarURL)
-                .setTimestamp();
 
             return message.channel.send(helpEmbed);
         } else {
