bd34c5ddfe4deae0bc430476f69c624ee276bf27
ArkaneOnline <niflheim.exease@gmail.com>
2020-04-23 18:48:28 -0500

    fix cache

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,7 @@ const package = require("../../package.json");
 module.exports = async (client) => {
     client.user.setPresence({ activity: { name: `${prefix}help | Early Access Build.`}, status: "dnd" });
     console.log(`${client.user.username} is online!`);
-    console.log(`${client.user.username} is currently in ${client.guilds.size} servers!`);
+    console.log(`${client.user.username} is currently in ${client.guilds.cache.size} servers!`);
     console.log(`${client.user.username} is currently in version ${package.version}`);
     console.log(`${client.user.username} uses the following packages:`);
     console.log(package.dependencies);
