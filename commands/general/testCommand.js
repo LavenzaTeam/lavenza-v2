22123e3bb06af76dc50dbfb18a5017992606798a
ArkaneOnline <niflheim.exease@gmail.com>
2020-04-23 19:14:16 -0500

    test command

---
+++ commands/general/testCommand.js
@@ -0,0 +1,15 @@
+config { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    config: {
+        name: "testcommand",
+        aliases: "",
+        description: "",
+        usage: "",
+        category: "general"
+    },
+    run: async (client, message, args) => {
+        let url = client.user.displayAvatarURL;
+        return message.reply(`yes ${url}`);
+    }
+}
