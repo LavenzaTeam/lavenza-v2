4362c8786906249cc53afb78f8337e75e4814a51
ArkaneOnline <niflheim.exease@gmail.com>
2020-08-27 10:14:39 -0500

    update status

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const { prefix } = require("../../config.json");
 const package = require("../../package.json");
 
 module.exports = async (client) => {
-    client.user.setPresence({ activity: { name: `${prefix}help | Early Access Build.`}, status: "dnd" });
+    client.user.setPresence({ activity: { name: `${prefix}help | Early Preview, what you see right now does not reflect the final product.`}, status: "dnd" });
     console.log(`${client.user.username} is online!`);
     console.log(`${client.user.username} is currently in ${client.guilds.cache.size} servers!`);
     console.log(`${client.user.username} is currently in version ${package.version}`);
